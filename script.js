b562e0eea129868de1eb71cd859a45564ab0a0d7
Wabuko <wabuko.akhanyinya@student.moringaschool.com>
2021-08-01 08:30:00 -0700

    adding the deliveries section

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+alert(123);
 function getTotal() {
     let num1 = parseInt(document.getElementById("size").value)
     document.getElementById('sizea').innerHTML = num1
@@ -13,119 +13,131 @@ function getTotal() {
     document.getElementById('toppinga').innerHTML = num4
     let topping = document.getElementById("toppings").value
     let total = parseInt(((num1 + num2 + num4) * num3))
+    document.getElementById("totala").innerHTML = total
     alert(total)
     if (num1 === 500 && num2 === 150 && num4 == 100) {
-    alert("You have placed an order of" + " " + num3 + " " + "small" + " " + "crispy" + " " + "crust" + " " + "pepperoni" + " " + "pizza")
-    
+        alert("You have placed an order of" + " " + num3 + " " + "small" + " " + "crispy" + " " + "crust" + " " + "pepperoni" + " " + "pizza")
+
     } else if (num1 === 500 && num2 === 150 && num4 == 150) {
-    alert("You have placed an order of" + " " + num3 + " " + "small" + " " + "crispy" + " " + "crust" + " " + "mushroom" + " " + "pizza")
+        alert("You have placed an order of" + " " + num3 + " " + "small" + " " + "crispy" + " " + "crust" + " " + "mushroom" + " " + "pizza")
     }
     else if (num1 === 500 && num2 === 150 && num4 == 200) {
-    alert("You have placed an order of" + " " + num3 + " " + "small" + " " + "crispy" + " " + "crust" + " " + "sausage" + " " + "pizza")
+        alert("You have placed an order of" + " " + num3 + " " + "small" + " " + "crispy" + " " + "crust" + " " + "sausage" + " " + "pizza")
     }
     else if (num1 === 500 && num2 === 250 && num4 == 100) {
-    alert("You have placed an order of" + " " + num3 + " " + "small" + " " + "stuffed" + " " + "crust" + " " + "pepperoni" + " " + "pizza")
+        alert("You have placed an order of" + " " + num3 + " " + "small" + " " + "stuffed" + " " + "crust" + " " + "pepperoni" + " " + "pizza")
     }
     else if (num1 === 500 && num2 === 250 && num4 == 150) {
-    alert("You have placed an order of" + " " + num3 + " " + "small" + " " + "stuffed" + " " + "crust" + " " + "mushrooms" + " " + "pizza")
+        alert("You have placed an order of" + " " + num3 + " " + "small" + " " + "stuffed" + " " + "crust" + " " + "mushrooms" + " " + "pizza")
     }
     else if (num1 === 500 && num2 === 250 && num4 == 200) {
-    alert("You have placed an order of" + " " + num3 + " " + "small" + " " + "stuffed" + " " + "crust" + " " + "sausage" + " " + "pizza")
+        alert("You have placed an order of" + " " + num3 + " " + "small" + " " + "stuffed" + " " + "crust" + " " + "sausage" + " " + "pizza")
     }
     else if (num1 === 500 && num2 === 200 && num4 == 100) {
-    alert("You have placed an order of" + " " + num3 + " " + "small" + " " + "gluten-free" + " " + "crust" + " " + "pepperoni" + " " + "pizza")
+        alert("You have placed an order of" + " " + num3 + " " + "small" + " " + "gluten-free" + " " + "crust" + " " + "pepperoni" + " " + "pizza")
     }
     else if (num1 === 500 && num2 === 200 && num4 == 150) {
-    alert("You have placed an order of" + " " + num3 + " " + "small" + " " + "gluten-free" + " " + "crust" + " " + "mushrooms" + " " + "pizza")
+        alert("You have placed an order of" + " " + num3 + " " + "small" + " " + "gluten-free" + " " + "crust" + " " + "mushrooms" + " " + "pizza")
     }
     else if (num1 === 500 && num2 === 200 && num4 == 200) {
-    alert("You have placed an order of" + " " + num3 + " " + "small" + " " + "gluten-free" + " " + "crust" + " " + "sausage" + " " + "pizza")
+        alert("You have placed an order of" + " " + num3 + " " + "small" + " " + "gluten-free" + " " + "crust" + " " + "sausage" + " " + "pizza")
     }
     else if (num1 === 900 && num2 === 150 && num4 == 150) {
-    alert("You have placed an order of" + " " + num3 + " " + "medium" + " " + "crispy" + " " + "crust" + " " + "mushroom" + " " + "pizza")
+        alert("You have placed an order of" + " " + num3 + " " + "medium" + " " + "crispy" + " " + "crust" + " " + "mushroom" + " " + "pizza")
     }
     else if (num1 === 900 && num2 === 150 && num4 == 200) {
-    alert("You have placed an order of" + " " + num3 + " " + "medium" + " " + "crispy" + " " + "crust" + " " + "sausage" + " " + "pizza")
+        alert("You have placed an order of" + " " + num3 + " " + "medium" + " " + "crispy" + " " + "crust" + " " + "sausage" + " " + "pizza")
     }
     else if (num1 === 900 && num2 === 250 && num4 == 100) {
-    alert("You have placed an order of" + " " + num3 + " " + "medium" + " " + "stuffed" + " " + "crust" + " " + "pepperoni" + " " + "pizza")
+        alert("You have placed an order of" + " " + num3 + " " + "medium" + " " + "stuffed" + " " + "crust" + " " + "pepperoni" + " " + "pizza")
     }
     else if (num1 === 900 && num2 === 250 && num4 == 150) {
-    alert("You have placed an order of" + " " + num3 + " " + "medium" + " " + "stuffed" + " " + "crust" + " " + "mushrooms" + " " + "pizza")
+        alert("You have placed an order of" + " " + num3 + " " + "medium" + " " + "stuffed" + " " + "crust" + " " + "mushrooms" + " " + "pizza")
     }
     else if (num1 === 900 && num2 === 250 && num4 == 200) {
-    alert("You have placed an order of" + " " + num3 + " " + "medium" + " " + "stuffed" + " " + "crust" + " " + "sausage" + " " + "pizza")
+        alert("You have placed an order of" + " " + num3 + " " + "medium" + " " + "stuffed" + " " + "crust" + " " + "sausage" + " " + "pizza")
     }
     else if (num1 === 900 && num2 === 200 && num4 == 100) {
-    alert("You have placed an order of" + " " + num3 + " " + "medium" + " " + "gluten-free" + " " + "crust" + " " + "pepperoni" + " " + "pizza")
+        alert("You have placed an order of" + " " + num3 + " " + "medium" + " " + "gluten-free" + " " + "crust" + " " + "pepperoni" + " " + "pizza")
     }
     else if (num1 === 900 && num2 === 200 && num4 == 150) {
-    alert("You have placed an order of" + " " + num3 + " " + "medium" + " " + "gluten-free" + " " + "crust" + " " + "mushrooms" + " " + "pizza")
+        alert("You have placed an order of" + " " + num3 + " " + "medium" + " " + "gluten-free" + " " + "crust" + " " + "mushrooms" + " " + "pizza")
     }
     else if (num1 === 900 && num2 === 200 && num4 == 200) {
-    alert("You have placed an order of" + " " + num3 + " " + "medium" + " " + "gluten-free" + " " + "crust" + " " + "sausage" + " " + "pizza")
+        alert("You have placed an order of" + " " + num3 + " " + "medium" + " " + "gluten-free" + " " + "crust" + " " + "sausage" + " " + "pizza")
     }
     else if (num1 === 900 && num2 === 150 && num4 == 100) {
-    alert("You have placed an order of" + " " + num3 + " " + "medium" + " " + "crispy" + " " + "crust" + " " + "pepperoni" + " " + "pizza")
+        alert("You have placed an order of" + " " + num3 + " " + "medium" + " " + "crispy" + " " + "crust" + " " + "pepperoni" + " " + "pizza")
     }
     else if (num1 === 1200 && num2 === 150 && num4 == 100) {
-    alert("You have placed an order of" + " " + num3 + " " + "large" + " " + "crispy" + " " + "crust" + " " + "pepperoni" + " " + "pizza")
-    
+        alert("You have placed an order of" + " " + num3 + " " + "large" + " " + "crispy" + " " + "crust" + " " + "pepperoni" + " " + "pizza")
+
     } else if (num1 === 1200 && num2 === 150 && num4 == 150) {
-    alert("You have placed an order of" + " " + num3 + " " + "large" + " " + "crispy" + " " + "crust" + " " + "mushroom" + " " + "pizza")
+        alert("You have placed an order of" + " " + num3 + " " + "large" + " " + "crispy" + " " + "crust" + " " + "mushroom" + " " + "pizza")
     }
     else if (num1 === 1200 && num2 === 150 && num4 == 200) {
-    alert("You have placed an order of" + " " + num3 + " " + "large" + " " + "crispy" + " " + "crust" + " " + "sausage" + " " + "pizza")
+        alert("You have placed an order of" + " " + num3 + " " + "large" + " " + "crispy" + " " + "crust" + " " + "sausage" + " " + "pizza")
     }
     else if (num1 === 1200 && num2 === 250 && num4 == 100) {
-    alert("You have placed an order of" + " " + num3 + " " + "large" + " " + "stuffed" + " " + "crust" + " " + "pepperoni" + " " + "pizza")
+        alert("You have placed an order of" + " " + num3 + " " + "large" + " " + "stuffed" + " " + "crust" + " " + "pepperoni" + " " + "pizza")
     }
     else if (num1 === 1200 && num2 === 250 && num4 == 150) {
-    alert("You have placed an order of" + " " + num3 + " " + "large" + " " + "stuffed" + " " + "crust" + " " + "mushrooms" + " " + "pizza")
+        alert("You have placed an order of" + " " + num3 + " " + "large" + " " + "stuffed" + " " + "crust" + " " + "mushrooms" + " " + "pizza")
     }
     else if (num1 === 1200 && num2 === 250 && num4 == 200) {
-    alert("You have placed an order of" + " " + num3 + " " + "large" + " " + "stuffed" + " " + "crust" + " " + "sausage" + " " + "pizza")
+        alert("You have placed an order of" + " " + num3 + " " + "large" + " " + "stuffed" + " " + "crust" + " " + "sausage" + " " + "pizza")
     }
     else if (num1 === 1200 && num2 === 200 && num4 == 100) {
-    alert("You have placed an order of" + " " + num3 + " " + "large" + " " + "gluten-free" + " " + "crust" + " " + "pepperoni" + " " + "pizza")
+        alert("You have placed an order of" + " " + num3 + " " + "large" + " " + "gluten-free" + " " + "crust" + " " + "pepperoni" + " " + "pizza")
     }
     else if (num1 === 1200 && num2 === 200 && num4 == 150) {
-    alert("You have placed an order of" + " " + num3 + " " + "large" + " " + "gluten-free" + " " + "crust" + " " + "mushrooms" + " " + "pizza")
+        alert("You have placed an order of" + " " + num3 + " " + "large" + " " + "gluten-free" + " " + "crust" + " " + "mushrooms" + " " + "pizza")
     }
     else if (num1 === 1200 && num2 === 200 && num4 == 200) {
-    alert("You have placed an order of" + " " + num3 + " " + "large" + " " + "gluten-free" + " " + "crust" + " " + "sausage" + " " + "pizza")
+        alert("You have placed an order of" + " " + num3 + " " + "large" + " " + "gluten-free" + " " + "crust" + " " + "sausage" + " " + "pizza")
     }
+}
+function delivery(){
+    let jina = document.getElementById("jina").value
+    alert (jina)
+    let mahali = document.getElementById("mahali").value
+    alert(mahali)
+}
+function chekout() {
+    if (document.getElementById('sizea').innerHTML == 1200) {
+        document.getElementById("size-result").innerHTML = 'large'
     }
-    function chekout(){
-    if(document.getElementById('sizea').innerHTML== 1200){
-    document.getElementById("size-result").innerHTML = 'large'
-    alert(123)
+    if (document.getElementById('sizea').innerHTML == 900) {
+        document.getElementById("size-result").innerHTML = 'medium'
     }
-    if(document.getElementById('sizea').innerHTML== 900){
-    document.getElementById("size-result").innerHTML = 'medium'
-    alert(123)
+    if (document.getElementById('sizea').innerHTML == 500) {
+        document.getElementById("size-result").innerHTML = 'small'
     }
-    if(document.getElementById('sizea').innerHTML== 500){
-    document.getElementById("size-result").innerHTML = 'small'
-    alert(123)
+    if (document.getElementById("crusta").innerHTML == 150) {
+        document.getElementById("crusty").innerHTML = 'crispy'
     }
-    if(document.getElementById("crusta").innerHTML ==150){
-    document.getElementById("crusty").innerHTML = 'crispy'
+    if (document.getElementById("crusta").innerHTML == 250) {
+        document.getElementById("crusty").innerHTML = 'stuffed'
     }
-    if(document.getElementById("crusta").innerHTML ==250){
-    document.getElementById("crusty").innerHTML = 'stuffed'
+    if (document.getElementById("crusta").innerHTML == 200) {
+        document.getElementById("crusty").innerHTML = 'glutten-free'
     }
-    if(document.getElementById("crusta").innerHTML ==200){
-    document.getElementById("crusty").innerHTML = 'glutten-free'
-    }
-    if(document.getElementById('toppinga').innerHTML == 100){
+    if (document.getElementById('toppinga').innerHTML == 100) {
         document.getElementById('toppingb').innerHTML = 'pepperoni'
     }
-    if(document.getElementById('toppinga').innerHTML == 150){
+    if (document.getElementById('toppinga').innerHTML == 150) {
         document.getElementById('toppingb').innerHTML = 'mushrooms'
     }
-   else if(document.getElementById('toppinga').innerHTML == 200){
+    if (document.getElementById('toppinga').innerHTML == 200) {
         document.getElementById('toppingb').innerHTML = 'sausage'
     }
-    }
     
+}
+function delivery(){
+   let myLoc= prompt('Please enter your location ')
+   let myNam = prompt('Please enter your name ')
+   
+   
+    
+
+}
